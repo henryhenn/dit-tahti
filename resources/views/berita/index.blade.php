c14a7c337f11fe5791e39e9ca93961ed29c36e50
= <=>
2023-05-29 20:23:11 +0700

    Fixed action button space in berita index & daftar barang index

--- resources/views/berita/index.blade.php
+++ resources/views/berita/index.blade.php
@@ -59,7 +59,7 @@
                                     <td class="px-6 py-4">
                                         {{$beritum->created_at->format('d M Y')}}
                                     </td>
-                                    <td class="px-6 py-4 flex justify-between">
+                                    <td class="px-6 py-4 flex flex-row">
                                         <a href="{{route('berita.edit', $beritum->id)}}"
                                            class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Edit
--- resources/views/daftar-barang/index.blade.php
+++ resources/views/daftar-barang/index.blade.php
@@ -71,7 +71,7 @@
                                     <td class="px-6 py-4">
                                         {{$barang->jenis_barang_bukti}}
                                     </td>
-                                    <td class="px-6 py-4 flex justify-between">
+                                    <td class="px-6 py-4 flex flex-row ">
                                         <a href="{{route('daftar-barang.edit', $barang->id)}}"
                                            class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Edit
