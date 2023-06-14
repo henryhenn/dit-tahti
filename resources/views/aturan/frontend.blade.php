8a4f62947f90d3646216e6061b967fc8bdb00322
= <=>
2023-06-14 20:17:17 +0700

    Fixed responsive issue FE aturan

--- resources/views/aturan/frontend.blade.php
+++ resources/views/aturan/frontend.blade.php
@@ -11,8 +11,7 @@
         <div class="relative hidden mt-2 mb-6 overflow-x-auto lg:block lg:px-40">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <tbody>
-                @foreach($aturan as $file)
-
+                @forelse($aturan as $file)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="py-4 pr-40 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{$file->judul}}
@@ -24,7 +23,11 @@
                             </a>
                         </td>
                     </tr>
-                @endforeach
+                @empty
+                    <tr>
+                        <td colspan="2"><h3 class="text-2xl font-bold text-center">Tidak ada aturan terbaru</h3></td>
+                    </tr>
+                @endforelse
                 </tbody>
             </table>
         </div>
@@ -33,70 +36,23 @@
 
 
     <div class="container block p-2 mx-auto lg:hidden">
-        <div class="flex w-full h-16 gap-2 p-2 mb-2 border rounded-md border-cyan-500">
-            <div class="w-72">
-                <h1 class="text-lg font-bold">ATURAN 1</h1>
-                <p class="text-xs">DITRESKRIMUM</p>
+        @forelse($aturan as $file)
+            <div class="flex w-full h-16 gap-2 p-2 mb-2 border rounded-md border-cyan-500">
+                <div class="w-72">
+                    <h1 class="text-lg font-bold">{{$file->judul}}</h1>
+                </div>
+                <div class="w-12 h-12 rounded-md bg-cyan-400">
+                    <a href="{{route('aturan.download', $file->id)}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="w-12 h-12 text-white">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
-            <div class="w-12 h-12 rounded-md bg-cyan-400">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" class="w-12 h-12 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
-                </svg>
-            </div>
-        </div>
-        <div class="flex w-full h-16 gap-2 p-2 mb-2 border rounded-md border-cyan-500">
-            <div class="w-72">
-                <h1 class="text-lg font-bold">ATURAN 2</h1>
-                <p class="text-xs">DITLANTAS</p>
-            </div>
-            <div class="w-12 h-12 rounded-md bg-cyan-400">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" class="w-12 h-12 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
-                </svg>
-            </div>
-        </div>
-        <div class="flex w-full h-16 gap-2 p-2 mb-2 border rounded-md border-cyan-500">
-            <div class="w-72">
-                <h1 class="text-lg font-bold">ATURAN 3</h1>
-                <p class="text-xs">DITRESKRIMSUS</p>
-            </div>
-            <div class="w-12 h-12 rounded-md bg-cyan-400">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" class="w-12 h-12 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
-                </svg>
-            </div>
-        </div>
-        <div class="flex w-full h-16 gap-2 p-2 mb-2 border rounded-md border-cyan-500">
-            <div class="w-72">
-                <h1 class="text-lg font-bold">ATURAN 4</h1>
-                <p class="text-xs">DITRESNARKOBA</p>
-            </div>
-            <div class="w-12 h-12 rounded-md bg-cyan-400">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" class="w-12 h-12 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
-                </svg>
-            </div>
-        </div>
-        <div class="flex w-full h-16 gap-2 p-2 mb-2 border rounded-md border-cyan-500">
-            <div class="w-72">
-                <h1 class="text-lg font-bold">ATURAN 5</h1>
-                <p class="text-xs">DITPOLAIRUD</p>
-            </div>
-            <div class="w-12 h-12 rounded-md bg-cyan-400">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" class="w-12 h-12 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
-                </svg>
-            </div>
-        </div>
+        @empty
+            <h2 class="text-center font-bold text-2xl">Tidak ada aturan terbaru</h2>
+        @endforelse
     </div>
 @endsection
