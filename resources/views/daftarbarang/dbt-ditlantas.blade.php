93588d7ff603a413b629d9012ecd0c22d094649d
= <=>
2023-06-18 21:33:42 +0700

    Fix ditlantas not found

--- resources/views/daftarbarang/dbt-ditlantas.blade.php
+++ resources/views/daftarbarang/dbt-ditlantas.blade.php
@@ -67,7 +67,7 @@
          </span>
             </a>
             <a
-                href="dbt/ditlantas"
+                href="/dbt/ditlantas"
                 class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-r from-orange-500 via-red-700 to-red-700 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
           <span
               class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
